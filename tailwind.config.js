1bc4172dbe504f4a4e310d0fad1dbc1511dc47e6
Mwelwa Nkuta <mwelwankuta@mail.com>
2021-04-23 18:34:24 -0700

    tried adding something

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
+  minify: false,
   theme: {
     extend: {},
     screens: {
@@ -23,5 +24,6 @@ module.exports = {
   variants: {
     extend: {},
   },
+
   plugins: [],
 };
