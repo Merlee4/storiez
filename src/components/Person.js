eb8d0d2394a3ba299c16a8ebf325325d49524ebd
Mwelwa Nkuta <mwelwankuta@mail.com>
2021-04-24 07:16:56 -0700

    added some ux features

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,7 +3,7 @@ export default function Person() {
     <div className="h-20 w-12 flex flex-col items-center p-1 cursor-pointer group relative">
       <div className="relative">
         <img
-          src="https://m.media-amazon.com/images/I/81B5c6m0jaL._SS500_.jpg"
+          src="https://www.itsretunes.com/trinah_files/retunes_Trinah_no_matter.jpg"
           className="rounded-full h-full w-full"
           alt="person"
         />
